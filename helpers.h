82031060b23a2ad5b9af04a7dc8c030f78769ad9
hypRe1 <james.fenwick@mac.com>
2024-01-05 08:00:09 +0000

    move encoding and decoding

--- helpers.h
+++ helpers.h
@@ -42,16 +42,29 @@ U64 flipVertical(U64 bitboard) {
 // special: 2 bits
 // heuristc: 10 bits
 
-#define encode_move(source, target, piece, promoted, capture, double, enpassant, castling) \
-    ((source) | (target << 6) | (piece << 12) | (capture << 16) | (double << 20) | (castling << 21) | (promoted << 22) | (enpassant << 23))
-#define get_move_source(move) (move & 0x3f)
-#define get_move_target(move) ((move & 0xfc0) >> 6)
-#define get_move_piece(move) ((move & 0xf000) >> 12)
-#define get_move_capture(move) ((move & 0xf0000) >> 16)
-#define get_move_double(move) (move & 0x100000)
-#define get_move_castling(move) (move & 0x200000)
-#define get_move_promoted(move) (move & 0x400000)
-#define get_move_enpassant(move) (move & 0x800000)
+// 0000 0000 0000 0000 0000 0000 0011 1111 source
+// 0000 0000 0000 0000 0000 1111 1100 0000 target
+// 0000 0000 0000 0000 0111 0000 0000 0000 sourcep
+// 0000 0000 0000 0011 1000 0000 0000 0000 targetp
+// 0000 0000 0000 0100 0000 0000 0000 0000 promotion
+// 0000 0000 0000 1000 0000 0000 0000 0000 capture
+// 0000 0000 0011 0000 0000 0000 0000 0000 special
+// 0000 0000 0011 1100 0000 0000 0000 0000 code
+// 1111 1111 1100 0000 0000 0000 0000 0000 heuristic
+
+#define encode_move(source, target, sourcepiece, targetpiece, promotion, capture, special, heuristic) \
+    ((source) | ((target) << 6) | ((sourcepiece) << 12) | ((targetpiece) << 15) | ((promotion) << 18) | ((capture) << 19) | ((special) << 20) | ((heuristic << 22)))
+
+#define get_move_source(move)       ((move) & 0x3f)
+#define get_move_target(move)       (((move) & 0xfc0) >> 6)
+#define get_move_sourcep(move)      (((move) & 0x7000) >> 12)
+#define get_move_targetp(move)      (((move) & 0x38000) >> 15)
+#define get_move_promotion(move)    ((move) & 0x40000)
+#define get_move_capture(move)      ((move) & 0x80000)
+#define get_move_special(move)      (((move) & 0x300000) >> 20)
+#define get_move_code(move)         (((move) & 0x3c0000) >> 18)
+#define get_move_heuristic(move)    (((move) & 0xffc00000) >> 22)
+
 
 // https://stackoverflow.com/questions/3585846/color-text-in-terminal-applications-in-unix
 const char version[] = "\x1B[36mv0.0.1\x1B[0m";
@@ -353,4 +366,72 @@ void print_board(struct Board* board) {
         printf("\n");
     }
     printf("\n    a b c d e f g h\n\n");
+}
+
+void print_move(unsigned int move) {
+    int from = get_move_source(move);
+    int to = get_move_target(move);
+    int code = get_move_code(move);
+
+    printf("%s%s", square_to_coordinates[from], square_to_coordinates[to]);
+
+    switch (code) {
+        case (0): {  // Quiet move
+            printf(" Quiet move %s\n", ascii_pieces[get_move_sourcep(move)]);
+            break;
+        }
+        case (1): {   // Double pawn push
+            printf(" Double pawn push\n");
+            break;
+        }
+        case (2): {   // King castle
+            printf(" Kingside castle\n");
+            break;
+        }
+        case (3): {   // Queen castle
+            printf(" Queenside castle\n");
+            break;
+        }
+        case (4): {   // Captures
+            printf(" %s captured %s\n", ascii_pieces[get_move_sourcep(move)], ascii_pieces[get_move_targetp(move)]);
+            break;
+        }
+        case (5): {   // EP-capture
+            printf(" Enpassant\n");
+            break;
+        }
+        case (6): {   // Knight-promotion
+            printf("k\n");
+            break;
+        }
+        case (7): {   // Bishop-promotion
+            printf("b\n");
+            break;
+        }
+        case (8): {   // Rook-promotion
+            printf("r\n");
+            break;
+        }
+        case (9): {   // Queen-promotion
+            printf("q\n");
+            break;
+        }
+        case (10): {  // Knight-promotion capture
+            printf("n Captured %s\n", ascii_pieces[get_move_targetp(move)]);
+            break;
+        }
+        case (11): {  // Bishop-promotion capture
+            printf("b Captured %s\n", ascii_pieces[get_move_targetp(move)]);
+            break;
+        }
+        case (12): {  // Rook-promotion capture
+            printf("r Captured %s\n", ascii_pieces[get_move_targetp(move)]);
+            break;
+        }
+        case (13): {  // Queen-promotion capture
+            printf("q Captured %s\n", ascii_pieces[get_move_targetp(move)]);
+            break;
+        }
+    }
+
 }
